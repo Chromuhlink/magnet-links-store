80c89c0138c5cee1d46251522a70580db7ef5fa0
Amir Gamble <chromuh@ChromuhBook-Pro-Max.local>
2023-03-25 21:31:32 -0400

    another oneeee

--- src/pages/components/GoerliRatsProduct.tsx
+++ src/pages/components/GoerliRatsProduct.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const GoerliRatsProduct = ({ onBuyClick }) => {
     return (
         <div className="bg-white rounded-lg p-4 shadow-md">
-            <img src="product" alt="Goerli Rats FC Jersey" className="w-full h-64 object-cover" />
+            <img src="https://gateway.pinata.cloud/ipfs/QmSo3BLUCJyLmRBKat5cfthug4H6oiTTQdZi47aK7pdixP" alt="Goerli Rats FC Jersey" className="w-full h-64 object-cover" />
             <h2 className="font-bold text-xl mt-4">Goerli Rats FC Jersey</h2>
             <p className="text-gray-700">The most sought after jersey since testnet</p>
             <p className="font-bold text-lg mt-2">$75</p>
